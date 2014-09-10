0bbdd9d3a72976bfd1b8866e5a5f260ba7ce9f90
setzold <etzold.stefan@gmail.com>
2014-09-10 22:23:14 +0200

    added test class for addition

---
+++ ManipulateCommits.Tests/Calculation/TestAdd.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ManipulateCommits.Console.Calculation;
+namespace ManipulateCommits.Tests.Calculation
+{
+    [TestFixture]
+    public class TestAdd
+    {
+        [Test]
+        public void Test_Add_with_4_plus_5_expects_9()
+        {
+            var testClass = new Add();
+
+            int result = testClass.Plus(4, 5);
+
+            Assert.AreEqual(9, result, "return value was not 9 !!!");
+        }
+    }
+}
