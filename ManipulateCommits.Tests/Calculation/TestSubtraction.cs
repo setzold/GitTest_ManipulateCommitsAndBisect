69121f29a8ee716f37f94d17bebb44ed0c411b4b
setzold <etzold.stefan@gmail.com>
2014-09-10 23:32:13 +0200

    adding subtraction class and test

---
+++ ManipulateCommits.Tests/Calculation/TestSubtraction.cs
@@ -0,0 +1,19 @@
+﻿using ManipulateCommits.Console.Calculation;
+using NUnit.Framework;
+
+namespace ManipulateCommits.Tests.Calculation
+{
+    [TestFixture]
+    public class TestSubtraction
+    {
+        [Test]
+        public void Test_Subtraction_with_9_minus_4_expects_5()
+        {
+            var testClass = new Subtraction();
+
+            int result = testClass.Minus(9,4);
+
+            Assert.AreEqual(5, result, "return value was not 5 !!!");
+        }
+    }
+}
