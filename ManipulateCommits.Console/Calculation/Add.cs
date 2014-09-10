8e03399925c5a6cb9ac3ce03f86c3dee616f7ca3
setzold <etzold.stefan@gmail.com>
2014-09-10 22:23:45 +0200

    adding class for addition

---
+++ ManipulateCommits.Console/Calculation/Add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManipulateCommits.Console.Calculation
+{
+    public class Add
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
