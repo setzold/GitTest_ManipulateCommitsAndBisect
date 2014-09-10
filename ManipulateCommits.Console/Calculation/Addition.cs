3b321e08531622c3a72ced20f6f185d6d926a72a
setzold <etzold.stefan@gmail.com>
2014-09-10 23:25:30 +0200

    configuring nuget and adding build script

---
+++ ManipulateCommits.Console/Calculation/Addition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManipulateCommits.Console.Calculation
+{
+    public class Addition
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public decimal Plus(decimal a, decimal b)
+        {
+            return a + b;
+        }
+    }
+}
