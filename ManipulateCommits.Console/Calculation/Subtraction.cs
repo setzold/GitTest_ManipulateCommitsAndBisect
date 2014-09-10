135a753b904d396172a4d71168cbc57c8dd71917
setzold <etzold.stefan@gmail.com>
2014-09-10 23:32:13 +0200

    adding subtraction class and test

---
+++ ManipulateCommits.Console/Calculation/Subtraction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManipulateCommits.Console.Calculation
+{
+    public class Subtraction
+    {
+        public int Minus(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
