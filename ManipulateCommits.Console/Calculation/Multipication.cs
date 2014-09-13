f9296bae6d88d68800d6a0edeb33923a6471b560
setzold <etzold.stefan@gmail.com>
2014-09-13 16:18:20 +0200

    added class multiplication

---
+++ ManipulateCommits.Console/Calculation/Multipication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManipulateCommits.Console.Calculation
+{
+    class Multipication
+    {
+    }
+}
