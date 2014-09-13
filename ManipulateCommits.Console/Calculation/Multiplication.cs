ae14b1be9558abac8c1cfc503f85ebb79e747b86
setzold <etzold.stefan@gmail.com>
2014-09-13 16:22:02 +0200

    add renamed file multiplication.cs

---
+++ ManipulateCommits.Console/Calculation/Multiplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManipulateCommits.Console.Calculation
+{
+    public class Multiplication
+    {
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
