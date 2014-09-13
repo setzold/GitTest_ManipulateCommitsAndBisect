b5c29e1a42331f09ab934fdd2835b1219a7a9bcd
setzold <etzold.stefan@gmail.com>
2014-09-13 16:38:18 +0200

    adding class file for division tests

---
+++ ManipulateCommits.Tests/Calculation/TestDivision.cs
@@ -0,0 +1,19 @@
+﻿using ManipulateCommits.Console.Calculation;
+using NUnit.Framework;
+
+namespace ManipulateCommits.Tests.Calculation
+{
+    [TestFixture]
+    public class TestDivision
+    {
+        [Test]
+        public void Test_Divide_20_by_5_expects_4()
+        {
+            var testClass = new Division();
+
+            decimal result = testClass.divided(20, 5);
+
+            Assert.AreEqual(4, result, "return value was not  4!!!");
+        }
+    }
+}
