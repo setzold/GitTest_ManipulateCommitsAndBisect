7331172b8204f1ad675401e5224988592697e44a
setzold <etzold.stefan@gmail.com>
2014-09-13 16:26:15 +0200

    Adding testcalss and methods

---
+++ ManipulateCommits.Tests/Calculation/TestMultiplication.cs
@@ -0,0 +1,24 @@
+﻿using ManipulateCommits.Console.Calculation;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManipulateCommits.Tests.Calculation
+{
+    [TestFixture]
+    public class TestMultiplication
+    {
+        [Test]
+        public void Test_Multiply_4_by_5_expects_20()
+        {
+            var testClass = new Multiplication();
+
+            int result = testClass.Multiply(4, 5);
+
+            Assert.AreEqual(20, result, "return value was not 20 !!!");
+        }
+    }
+         
+}
